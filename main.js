e227d8b7f64f5cd2d1a1725f74b7d12ae8c32ff0
Quentin Chapon <q.chapon@gmail.com>
2021-07-16 16:01:06 +0200

    Corrected the Js

--- main.js
+++ main.js
@@ -12,30 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Envoi des données du formulaire
-document
-  .querySelector("#contact-form")
-  .addEventListener("submit", async (event) => {
-    // comportement par défaut sur le formulaire, annulé grâce à la fonction preventDefault
-    event.preventDefault();
-    console.log("Soumission du formulaire OK");
+document.querySelector("#form").addEventListener("submit", async (event) => {
+  // comportement par défaut sur le formulaire, annulé grâce à la fonction preventDefault
+  event.preventDefault();
+  console.log("Soumission du formulaire OK");
 
-    const data = {
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      email: document.querySelector("#email").value,
-      message: document.querySelector("#message").value,
-    };
+  const data = {
+    firstname: document.querySelector("#firstname").value,
+    lastname: document.querySelector("#lastname").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
 
-    //   console.log(data);
-    const response = await axios.post(
-      "https://tripadvisclone.netlify.app/",
-      data
-    );
-    console.log(response);
+  //   console.log(data);
+  const response = await axios.post(
+    "https://tripadvisclone.netlify.app/",
+    data
+  );
+  console.log(response);
 
-    if (response.status === 200) {
-      alert("Merci, tout va bien");
-    } else {
-      alert("Une erreur est survenue");
-    }
-  });
+  if (response.status === 200) {
+    alert("Merci, tout va bien");
+  } else {
+    alert("Une erreur est survenue");
+  }
+});
