e0fb3f8714aa671d0bd8258faf0542ef1d7998b3
Quentin Chapon <q.chapon@gmail.com>
2021-07-18 16:49:08 +0200

    added Axios

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     console.log(data);
     const response = await axios.post(
-      "https://tripadvisclone.netlify.app/form",
+      "https://tripadvisor-clone.herokuapp.com/",
       data
     );
 
