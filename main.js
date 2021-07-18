1c387e211a861dd057afbe6c8311a0a847107f75
Quentin Chapon <q.chapon@gmail.com>
2021-07-18 17:27:45 +0200

    added Axios

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       message: document.querySelector("#msg").value,
     };
     console.log(data);
-    const response = await axios.get(
+    const response = await axios.post(
       "https://tripadvisor-clone.herokuapp.com/",
       data
     );
